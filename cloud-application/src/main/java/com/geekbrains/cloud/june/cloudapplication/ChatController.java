e4064a74c27651c5cadf8df203fd9d032006331d
anasonov <agnason@gmail.com>
2022-06-12 22:16:02 +0300

    Homework#3 final

--- cloud-application/src/main/java/com/geekbrains/cloud/june/cloudapplication/ChatController.java
+++ cloud-application/src/main/java/com/geekbrains/cloud/june/cloudapplication/ChatController.java
@@ -5,29 +5,46 @@ import com.geekbrains.cloud.FileMessage;
 import com.geekbrains.cloud.FileRequest;
 import com.geekbrains.cloud.ListFiles;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class ChatController implements Initializable {
 
 
+    @FXML
+    public ComboBox<String> disksBox;// выбор диска
+    @FXML
+    public TextField pathField; // прописка пути файла
+    @FXML
+    public TableView<FileInfo> clientView;// поле клиента
+
+
     private String homeDir;
 
-    @FXML
-    public ListView<String> clientView;
+
+   // public ListView<String> clientView;
 
     @FXML
+    //TableView<String> serverView;
     public ListView<String> serverView;
 
     private Network network;
@@ -42,11 +59,11 @@ public class ChatController implements Initializable {
                         serverView.getItems().addAll(listFiles.getFiles());
                     });
                 } else if (message instanceof FileMessage fileMessage) {
-                    Path current = Path.of(homeDir).resolve(fileMessage.getName());
+                    Path current = Path.of(pathField.getText()).resolve(fileMessage.getName());
+                    System.out.println(current);
                     Files.write(current, fileMessage.getData());
                     Platform.runLater(() -> {
-                        clientView.getItems().clear();
-                        clientView.getItems().addAll(getFiles(homeDir));
+                        updateList(current.getParent());
                     });
                 }
             }
@@ -58,18 +75,91 @@ public class ChatController implements Initializable {
     // post init fx fields
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        // создаем в clientView столбец с определением файл это или директория
+        TableColumn<FileInfo, String> fileTypeColumn = new TableColumn<>();
+        fileTypeColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getType().getName()));
+        fileTypeColumn.setPrefWidth(24);
+        // создаем в clientView столбец с именем файла/директории
+        TableColumn<FileInfo, String> fileNameColumn = new TableColumn<>("Имя");
+        fileNameColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getFilename()));
+        fileNameColumn.setPrefWidth(200);
+        // создаем в clientView столбец с определением размера файла/директории
+        TableColumn<FileInfo, Long> fileSizeColumn = new TableColumn<>("Размер");
+        fileSizeColumn.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue().getSize()));
+        fileSizeColumn.setPrefWidth(120);
+
+        fileSizeColumn.setCellFactory(column -> {
+            return new TableCell<FileInfo, Long>() {
+                @Override
+                protected void updateItem(Long item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || empty) {
+                        setText(null);
+                        setStyle("");
+                    } else {
+
+                        String text = String.format("%,d bytes", item);
+                        if (item == -1L) {
+                            text = "DIR";
+                        }
+                        setText(text);
+
+                    }
+                }
+            };
+        });
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        TableColumn<FileInfo, String> fileDataColumn = new TableColumn<>("Дата изменения");
+        fileDataColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getLastModified().format(dtf)));
+        fileDataColumn.setPrefWidth(120);
+
+        clientView.getColumns().addAll(fileTypeColumn, fileNameColumn, fileSizeColumn, fileDataColumn);
+        clientView.getSortOrder().add(fileTypeColumn);
+
+        disksBox.getItems().clear();
+        for (Path p : FileSystems.getDefault().getRootDirectories()) {
+            disksBox.getItems().add(p.toString());
+        }
+        disksBox.getSelectionModel().select(0);
+// кликаем мышью и входим наверх
+        clientView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if(mouseEvent.getClickCount()==2) {
+                    Path path = Paths.get(pathField.getText()).resolve(clientView.getSelectionModel().getSelectedItem().getFilename());
+                    if(Files.isDirectory(path)){
+                        updateList(path);
+                    }
+                }
+            }
+        });
+
+        updateList(Paths.get("."));
         try {
-            homeDir = "client_files";
-            clientView.getItems().clear();
-            clientView.getItems().addAll(getFiles(homeDir));
+            // создается новый клиент, который подключается к "localhost" и c портом=8189
             network = new Network(8189);
             Thread readThread = new Thread(this::readLoop);
             readThread.setDaemon(true);
             readThread.start();
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+    }
+    public void updateList(Path path) {
+        try {
+            pathField.setText(path.normalize().toAbsolutePath().toString());
+            clientView.getItems().clear();
+            clientView.getItems().addAll(Files.list(path).map(FileInfo::new).toList());
+            clientView.sort();
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "По какой-то причине не удалось обновить списко файлов", ButtonType.OK);
+            alert.showAndWait();
         }
     }
+
 
     private List<String> getFiles(String dir) {
         String[] list = new File(dir).list();
@@ -77,13 +167,40 @@ public class ChatController implements Initializable {
         return Arrays.asList(list);
     }
 
+//    public void upload(ActionEvent actionEvent) throws IOException {
+//        String file = (clientView.getSelectionModel().getSelectedItem().getFilename());
+//        System.out.println(file);
+//        network.write(new FileMessage(Path.of("C:\\Users\\anasonov\\IdeaProjects\\Cloud\\geek-cloud-2022-june-master\\server_files").resolve(file)));
+//    }
+
     public void upload(ActionEvent actionEvent) throws IOException {
-        String file = clientView.getSelectionModel().getSelectedItem();
-        network.write(new FileMessage(Path.of(homeDir).resolve(file)));
+        Path path = Paths.get(pathField.getText()).resolve(clientView.getSelectionModel().getSelectedItem().getFilename());
+
+
+        network.write(new FileMessage(Path.of("server_files").resolve(path)));
     }
 
     public void download(ActionEvent actionEvent) throws IOException {
-        String file = serverView.getSelectionModel().getSelectedItem();
+        String file = (serverView.getSelectionModel().getSelectedItem());
         network.write(new FileRequest(file));
+    }
+
+    public void selectDiskAction(ActionEvent actionEvent) {
+    }
+// нажимаем на стрелку и у нас путь поднимается на один уровень вверх
+    public void btnPathUpRequest(ActionEvent actionEvent) {
+        Path upperPath = Paths.get(pathField.getText()).getParent();
+        if (upperPath !=null) {
+            updateList(upperPath);
+        }
+    }
+
+    public void btnExitAction(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+
+    public String getSelectedFilename() {
+
+        return pathField.getSelectedText();
     }
 }
--- cloud-server/src/main/java/com/geekbrains/cloud/netty/handler/CloudFileHandler.java
+++ cloud-server/src/main/java/com/geekbrains/cloud/netty/handler/CloudFileHandler.java
@@ -4,32 +4,40 @@ import com.geekbrains.cloud.CloudMessage;
 import com.geekbrains.cloud.FileMessage;
 import com.geekbrains.cloud.FileRequest;
 import com.geekbrains.cloud.ListFiles;
+import com.geekbrains.cloud.june.cloudapplication.FileInfo;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 
 public class CloudFileHandler extends SimpleChannelInboundHandler<CloudMessage> {
 
-    private Path currentDir;
+    private Path serverStorage;
 
     public CloudFileHandler() {
-        currentDir = Path.of("server_files");
+         serverStorage = Paths.get("C:\\Users\\anasonov\\IdeaProjects\\Cloud\\geek-cloud-2022-june-master\\server_files");
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        ctx.writeAndFlush(new ListFiles(currentDir));
+
+
+        ctx.writeAndFlush(new ListFiles(serverStorage));
+
     }
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, CloudMessage cloudMessage) throws Exception {
         if (cloudMessage instanceof FileRequest fileRequest) {
-            ctx.writeAndFlush(new FileMessage(currentDir.resolve(fileRequest.getName())));
+            ctx.writeAndFlush(new FileMessage(serverStorage.resolve(fileRequest.getName())));
+            System.out.println(serverStorage.resolve(fileRequest.getName()));
         } else if (cloudMessage instanceof FileMessage fileMessage) {
-            Files.write(currentDir.resolve(fileMessage.getName()), fileMessage.getData());
-            ctx.writeAndFlush(new ListFiles(currentDir));
+            Files.write(serverStorage.resolve(fileMessage.getName()), fileMessage.getData());
+            ctx.writeAndFlush(new ListFiles(serverStorage));
         }
     }
 }
